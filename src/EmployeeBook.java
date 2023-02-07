a31525385f75e64b4863f25063c4817feb33959d
Elena Koroleva <koroliana@yandex.ru>
2023-02-07 20:12:07 +0700

    Added methods summarizeSalaries and findMinSalaryEmployee

--- src/EmployeeBook.java
+++ src/EmployeeBook.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class EmployeeBook {
-    private static ArrayList <Employee> base = new ArrayList <Employee>();
+    private static final ArrayList <Employee> base = new ArrayList<>();
 
     public static void addEmloyee(Employee employee) {
         base.add(employee);
@@ -16,10 +16,29 @@ public class EmployeeBook {
         return base;
     }
 
-    public static void printBase () {
-        for (int i = 0; i < base.size(); i++) {
-            System.out.println(base.get(i));
+    public static void printBase() {
+        for (Employee employee : base) {
+            System.out.println(employee);
         }
+    }
+
+    public static int summarizeSalaries() {
+        int sum = 0;
+        for (Employee employee : base) {
+            sum = sum + employee.getSalary();
+        }
+        return sum;
+    }
+
+    public static Employee findMinSalaryEmployee() {
+        Employee minSalaryEmployee = base.get(0);
+        for (int i = 1; i < base.size(); i++) {
+            Employee employee = base.get(i);
+            if(employee.getSalary() < minSalaryEmployee.getSalary()) {
+                minSalaryEmployee = employee;
+            }
+        }
+        return minSalaryEmployee;
     }
 
 
--- src/Launch.java
+++ src/Launch.java
@@ -3,7 +3,12 @@ public class Launch {
         EmployeeBook.addEmloyee("Васичкин Василий",  8, 200_000);
         Employee employee1 = new Employee("Василиса Прекрасная Надеждовна", 1, 145_000);
         EmployeeBook.addEmloyee(employee1);
+        EmployeeBook.addEmloyee("Александрова Александра",  9, 205_000);
         EmployeeBook.printBase();
+        System.out.println();
+        System.out.println("Сумма затрат на ЗП: " + EmployeeBook.summarizeSalaries());
+        System.out.println();
+        System.out.println("Сотрудник с минимальной ЗП - " + EmployeeBook.findMinSalaryEmployee());
 
     }
 }
